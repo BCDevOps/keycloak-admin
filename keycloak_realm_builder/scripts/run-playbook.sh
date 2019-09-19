62558cbe686389930619b2cc62df12328d055e0e
ShellyXueHan <shelly.han@gov.bc.ca>
2019-09-19 11:52:04 -0700

    pass lable name to playbook

--- keycloak_realm_builder/scripts/run-playbook.sh
+++ keycloak_realm_builder/scripts/run-playbook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ansible-playbook playbook.yml -e repo_url=$1 -e branch=$2 -e pull_request_number=$3 \
--e repo_owner=$4 -e pull_request_url=$5 -e issue_url=$6 \
+-e repo_owner=$4 -e pull_request_url=$5 -e issue_url=$6 -e label_name=$7  \
 -e gh_token=$TOKEN
