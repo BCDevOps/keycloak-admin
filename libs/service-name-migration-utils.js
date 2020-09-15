7cfa4bf89e4577b60a04cf84354cbec380a90bd5
ShellyXueHan <shelly.han@gov.bc.ca>
2020-09-15 16:36:11 -0700

    added function to update idp mappers

--- libs/service-name-migration-utils.js
+++ libs/service-name-migration-utils.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { IDP_REF, CLIENT_MIGRATION_FIELDS } = require('../constants');
+const { IDP_REF, CLIENT_MIGRATION_FIELDS, IDP_MAPPER_TYPES, IDP_USER_ATTRI_MAPPERS } = require('../constants');
 
 /**
  * Replace target string in json object and return the new object
@@ -27,9 +27,9 @@ const urlReplacer = (jsonContent, targetString, replacement) => {
  */
 const updateAppRealmIdp = async (appRealmName, idp, kcAdminClient, newRoute, oldRoute) => {  
   try {    
-    // github example:
+    // Set target IDP and realm name:
     const targetRealm = appRealmName;
-    const targetIdp = IDP_REF[idp];
+    const targetIdp = IDP_REF[idp.toUpperCase()];
 
     // Find IDP based on realm name and IDP alias:
     const idpRef = {
@@ -44,7 +44,8 @@ const updateAppRealmIdp = async (appRealmName, idp, kcAdminClient, newRoute, old
     const newIdp = urlReplacer(idpConfig, oldRoute, newRoute);
 
     // 3. update the idp:
-    await kcAdminClient.identityProviders.update(idpRef, newIdp);
+    console.log(newIdp);
+    // await kcAdminClient.identityProviders.update(idpRef, newIdp);
 
   } catch (e) {
     throw e;
@@ -98,7 +99,8 @@ const updateIdpAppClient = async (clientId, idp, kcAdminClient, newRoute, oldRou
     }, {});
 
     // 3. update the idp client:
-    await kcAdminClient.clients.update(idpClientRef, newIdpClient);
+    console.log(newIdpClient);
+    // await kcAdminClient.clients.update(idpClientRef, newIdpClient);
 
     // 4. TODO - after confirming changes are good, delete the extra redirect uri from step 2.2
 
@@ -107,4 +109,40 @@ const updateIdpAppClient = async (clientId, idp, kcAdminClient, newRoute, oldRou
   }
 }
 
-module.exports = { urlReplacer, updateIdpAppClient, updateAppRealmIdp };
+/**
+ * Update Agent IDP Mapper Configurations to use new Federation Services
+ * @param {kcAdmin} kcAdminClient with auth setup
+ * @param {Object} mapperContent the old mapper config
+ * @param {Object} mapperRef the old mapper reference
+ */
+const updateAgentIdpMappers = async (kcAdminClient, mapperContent, mapperRef) => {
+  let newMapperContent = mapperContent;
+
+  // Mappers changed from the new federation services - all attributes are now lower case:
+  const newMapperFormat = {
+    config: { template: '${ATTRIBUTE.username}' },
+  };
+
+  if (mapperContent.identityProviderMapper == IDP_MAPPER_TYPES[0]) {
+    // 1. username mapper:
+    newMapperContent.config = newMapperFormat.config;
+    console.log(newMapperContent.config);
+    // await kcAdminClient.identityProviders.updateMapper(mapperRef, newMapperContent);
+
+  } else if (mapperContent.identityProviderMapper == IDP_MAPPER_TYPES[1]) {
+    // 2. user attribute mapper:
+    // only update the user attribute mappers changed from new services
+    const mapperAttri = mapperContent.config['user.attribute'];
+    const mapperAttriName = mapperContent.config['attribute.name'];
+
+    if (IDP_USER_ATTRI_MAPPERS.includes(mapperAttri.toLowerCase())) {
+      newMapperContent.config['attribute.name'] = mapperAttriName.toLowerCase();
+      console.log(newMapperContent.config);
+      // await kcAdminClient.identityProviders.updateMapper(mapperRef, newMapperContent);
+    }
+  } else {
+    // 3. Don't care
+  }
+}
+
+module.exports = { urlReplacer, updateIdpAppClient, updateAppRealmIdp, updateAgentIdpMappers };
