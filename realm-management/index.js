640d05169963f3bd43f49ae6254b86e3f74a3a06
ShellyXueHan <shelly.han@gov.bc.ca>
2021-01-04 11:39:20 -0800

    formatting

--- realm-management/index.js
+++ realm-management/index.js
@@ -9,23 +9,23 @@ const { KC_CONFIG, KC_TERMS } = require('./constants');
 
 const main = async () => {
   try {
-    // setup kc admin instance:
+    // +++ Setup kc admin instance:
     // eslint-disable-next-line new-cap
     const kcAdminClient = new kcAdmin(KC_CONFIG.REQUEST);
     await kcAdminClient.auth(KC_CONFIG.AUTH);
 
-    // Override client configuration for all further requests:
+    // +++ Override client configuration for all further requests:
     // kcAdminClient.setConfig({
     //   realmName: KC_CONFIG.REALM.NAME,
     // });
 
-    // Get all realms:
+    // +++ Get all realms:
     // const allRealms = await getAllRealms(kcAdminClient);
     // allRealms.forEach((r) => {
     //   console.log(`ID: ${r.id}, Realm Name: ${r.displayName}`);
     // });
 
-    // Get all users:
+    // +++ Get all users:
     // const userCount = await allRealms.reduce(async (acc, r) => {
     //   const userInRealm = await acc;
     //   console.log(r.realm);
@@ -45,17 +45,17 @@ const main = async () => {
     // console.log(`Total users: ${totalCount} for ${realms.length} realms.`);
     // await fs.outputJson(`tmp/${process.env.KC_ENV}-users-${totalCount}-realm-${realms.length}.json`, userCount);
 
-    // Pre-populate users from LDAP, require realm setup already:
+    // +++ Pre-populate users from LDAP, require realm setup already:
     // await importLdapUsers();
     // await createUsers(kcAdminClient);
 
-    // SSO Service Name Migration
+    // +++ SSO Service Name Migration
     // await ssoServiceNameMigration(kcAdminClient);
 
-    // Backup realm settings:
+    // +++ Backup realm settings:
     // await getRealmSettings(kcAdminClient, KC_CONFIG.REALM.NAME);
 
-    // Disable impersonation role:
+    // +++ Disable impersonation role:
     // await deleteClientRole(KC_TERMS.IMPERSONATION_ROLE, false);
   } catch (err) {
     throw Error(err);
